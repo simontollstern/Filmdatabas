4de206c386713d4c298c45ddc48a520f5419788c
simontollstern <simoon909@hotmail.com>
2016-10-24 14:39:08 +0200

    Lagt till formulär för att mata in nya filmer - fungerar inte än. Börjat lägga till data i tabellen.

--- src/filmdatabas/FXMLDocumentController.java
+++ src/filmdatabas/FXMLDocumentController.java
@@ -1,26 +1,47 @@
 package filmdatabas;
 
+import com.mysql.jdbc.Connection;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
 
 public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    Pane parentPane;
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    Button bgBtn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        try {
+            Connection conn = ConnectionFactory.getConnection();
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM movies";
+            ResultSet db = stmt.executeQuery(sql);
+
+            while (db.next()) {
+                String output = String.format("%s", db.getString("name"));
+                System.out.println(output);
+            }
+        } catch (SQLException ex) {
+            System.out.println("DATABASE ERROR");
+        }
+        
+        Form inputForm = new Form();
+
+        bgBtn.setOnAction((event) -> {
+            parentPane.getChildren().add(inputForm);
+        });
+    }
 }
--- src/filmdatabas/Filmdatabas.java
+++ src/filmdatabas/Filmdatabas.java
@@ -1,9 +1,5 @@
 package filmdatabas;
 
-import com.mysql.jdbc.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,36 +8,23 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Filmdatabas extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        
+
         stage.initStyle(StageStyle.DECORATED);
         stage.setTitle("movieDB");
-        
-        Scene scene = new Scene(root);      
+
+        Scene scene = new Scene(root);
         stage.setScene(scene);
-        
+
         scene.getStylesheets().add(this.getClass().getResource("style.css").toExternalForm());
         stage.show();
     }
 
     public static void main(String[] args) {
-        try {
-            Connection conn = ConnectionFactory.getConnection();
-            Statement stmt = conn.createStatement();
-            String sql = "SELECT * FROM movies";
-            ResultSet data = stmt.executeQuery(sql);
-            
-            while(data.next()){
-                String output = String.format("%s kostar %d kr.", data.getString("namn"), data.getInt("pris"));
-                System.out.println(output);
-            }
-        } catch (SQLException ex) {
-            System.out.println("DATABASE ERROR");
-        }
+        launch(args);
     }
-    
+
 }
